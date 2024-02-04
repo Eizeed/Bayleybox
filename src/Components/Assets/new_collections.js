73c6fe43298e734c80a7c7a62435a0a8fe4f493d
Евгений Тен <eizeed321@gmail.com>
2024-02-04 23:05:52 +0900

    title change

--- src/Components/Assets/new_collections.js
+++ src/Components/Assets/new_collections.js
@@ -1,5 +1,8 @@
 import img1 from './1.jpg'
 import img2 from './2.jpg'
+import img3 from './3.jpg'
+import img4 from './4.jpg'
+import img5 from './5.jpg'
 
 let new_collections = [
   {
@@ -8,7 +11,7 @@ let new_collections = [
     image: img1,
     new_price: 30000,
     category: 'kids',
-    desciption: 'Small white brick house. Easy to build, yet equally engaging acquisition for your child',
+    description: 'Small white brick house. Easy to build, yet equally engaging acquisition for your child',
     tags: ['For kids', 'Small building', 'House'],
   },
   {
@@ -16,9 +19,36 @@ let new_collections = [
     name: "Boys Orange Colourblocked Hooded Sweatshirt",
     image: img2,
     new_price: 35000,
-    category: 'kids',
-    desciption: 'Small white brick house. Easy to build, yet equally engaging acquisition for your child',
+    category: 'adults',
+    description: 'A medium-sized store. It boasts an abundance of details that accentuate the beauty of this building',
     tags: ['For kids', 'Medium building', 'Shop'],
+  },
+  {
+    id: 3,
+    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    image: img3,
+    new_price: 6000,
+    category: 'kids',
+    description: 'A small kit will allow you to create a candle holder in the form of a fireplace with your own hands',
+    tags: ['For kids', 'Small building', 'Decoration'],
+  },
+  {
+    id: 4,
+    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    image: img4,
+    new_price: 32000,
+    category: 'adults',
+    description: 'Average-sized house with a yard and a doghouse.',
+    tags: ['For kids', 'Medium building', 'House'],
+  },
+  {
+    id: 5,
+    name: "Boys Orange Colourblocked Hooded Sweatshirt",
+    image: img5,
+    new_price: 6000,
+    category: 'kids',
+    description: 'A well crafted from a small amount of material and makeshift tools',
+    tags: ['For kids', 'Small building', 'Decoration'],
   },
 ];
 
